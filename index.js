4cf8a30053399b5fabb185ce73643ae209ee8480
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-04-22 15:25:36 +0300

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+// function divide(num) {
+//     return num / 100;
+// }
+
+const divide = (a = 2000, b = 100) => a / b;
+
+const square = num => num * num;
+
+console.log(square(3));
+
+const add = (a, b) => a + b;
